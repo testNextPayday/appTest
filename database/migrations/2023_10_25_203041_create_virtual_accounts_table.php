2e0b7cb3be8db63caf99d9892f285b604ef221e4
barituleera <barituleera@gmail.com>
2023-10-25 21:26:10 +0100

    virtual account

--- database/migrations/2023_10_25_203041_create_virtual_accounts_table.php
+++ database/migrations/2023_10_25_203041_create_virtual_accounts_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('virtual_accounts', function (Blueprint $table) {
             $table->id();
+            $table->increments('id');
+            $table->bigInteger('user_id');
+            $table->string('number');
+            $table->string('bank');
+            $table->string('name');
+            $table->string('email');
+            $table->string('customer_code');
+            $table->string('currency');
+            $table->string('status');
+            $table->string('model')->default('user'); // user, investor
+            $table->timestamps();
             $table->timestamps();
         });
     }
