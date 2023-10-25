2e6eef09e31d5c19946343e81ebf1e071a1d973b
Bradley Yarrow <yarrowbradley@gmail.com>
2023-10-25 14:39:56 +0100

    Imported Auth Next payday

--- app/Http/Controllers/Users/EmploymentController.php
+++ app/Http/Controllers/Users/EmploymentController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
 use App\Services\ImageService;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
 class EmploymentController extends Controller
