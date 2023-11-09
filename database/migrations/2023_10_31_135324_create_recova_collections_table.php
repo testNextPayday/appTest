f2647384b1c45389266f772537813509b716d7b5
barituleera <barituleera@gmail.com>
2023-11-09 11:22:17 +0100

    migration fix

--- database/migrations/2023_10_31_135324_create_recova_collections_table.php
+++ database/migrations/2023_10_31_135324_create_recova_collections_table.php
@@ -1,34 +1,34 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('recova_collections', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('loan_reference');
-            $table->integer('debited_amount');
-            $table->integer('recovery_fee');
-            $table->integer('settlement_amount');
-            $table->string('transaction_reference');
-            $table->string('narration');
-            $table->string('institution_code');
-            $table->timestamps();
-        });
-    }
+// return new class extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      */
+//     public function up(): void
+//     {
+//         Schema::create('recova_collections', function (Blueprint $table) {
+//             $table->increments('id');
+//             $table->string('loan_reference');
+//             $table->integer('debited_amount');
+//             $table->integer('recovery_fee');
+//             $table->integer('settlement_amount');
+//             $table->string('transaction_reference');
+//             $table->string('narration');
+//             $table->string('institution_code');
+//             $table->timestamps();
+//         });
+//     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('recova_collections');
-    }
-};
+//     /**
+//      * Reverse the migrations.
+//      */
+//     public function down(): void
+//     {
+//         Schema::dropIfExists('recova_collections');
+//     }
+// };
